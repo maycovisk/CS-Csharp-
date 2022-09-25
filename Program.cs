8127f36c41d90c59759aad6aabf08163cff36b56
Maycovisk <90663036+maycovisk@users.noreply.github.com>
2022-09-25 20:14:06 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Digite seu nome: ");
+string name = Console.ReadLine();
+Console.WriteLine(name);
+Console.WriteLine("Olá " + name +"!");   //concatenar string
+Console.WriteLine($"Olá {name}!");       //concatenar string $
+
+Console.Write("Digite o ano do seu nascimento: ");
+int year = int.Parse(Console.ReadLine());
+int age = 2022 - year;
+Console.WriteLine($"Voce tem {age} anos!");
+
+if(age > 17){
+    Console.WriteLine("Você é maior de idade");
+ } else 
+    Console.WriteLine("Voce é menor de idade");
