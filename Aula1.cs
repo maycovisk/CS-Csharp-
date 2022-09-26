128b1bea38b19b4b046eca93b448f38541a04ce7
Maycovisk <90663036+maycovisk@users.noreply.github.com>
2022-09-25 21:01:07 -0300

    Add files via upload

---
+++ Aula1.cs
@@ -0,0 +1,20 @@
+﻿class Aula1
+{
+
+   static void Main()
+   {
+      string[] names = { "Maycon", "Clark"};
+
+      //Console.WriteLine(names[0]);
+      //Console.WriteLine(names[1]);
+
+      //for(int i = 0; i < 2; i++){
+      for(int i = 0; i < names.Length; i++){
+         Console.WriteLine(names[i]);
+      }
+
+      foreach(string name in names){    //PARA CADA STRING NAME EM NAMES O LUPE SERA EXECUTADO
+         Console.WriteLine(name);
+      }
+   }
+}
