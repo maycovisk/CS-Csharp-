22aa6b25e001bc5c766cf8cca526ade9223e7a4d
Maycon Lima <90663036+maycovisk@users.noreply.github.com>
2023-02-23 08:51:50 -0300

    Add files via upload

---
+++ Lista02/Lista02/Program.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lista01
+{
+    internal class Program
+    {
+        static void Exercicio1()
+        {
+            int[] num = new int[172];
+            Random randNum = new Random();
+
+            for (int i = 0; i < 172; i++)
+            {
+                num[i] = randNum.Next(23,9568); 
+            }
+
+            int menor = num[0];
+            int maior = num[0];
+            int soma = 0;
+            int qtdParesMenoresQue100 = 0;
+
+            for (int i = 0; i < 172; i++)
+            {
+                if (num[i] < menor)
+                {
+                    menor = num[i];
+                }
+                if (num[i] > maior)
+                {
+                    maior = num[i];
+                }
+                if (num[i] % 2 == 0 && num[i] < 100)
+                {
+                    soma += num[i];
+                    qtdParesMenoresQue100++;
+                }
+            }
+
+            int media = 0;
+            if (qtdParesMenoresQue100 > 0)
+            {
+                media = soma / qtdParesMenoresQue100;
+            }
+
+            Console.WriteLine("Menor valor: " + menor);
+            Console.WriteLine("Maior valor: " + maior);
+            Console.WriteLine("Média dos valores pares menores que 100: " + media);
+            Console.ReadKey();
+        }
+
+        static void Exercicio2()
+        {
+            int num;
+            int contador = 0;
+            int soma = 0;
+            double media = 0;
+
+            do
+            {
+                Console.Write("Numero {0}: ", contador + 1);
+                num = int.Parse(Console.ReadLine());
+
+                if(num >= 0)
+                {
+                    soma += num;
+                    contador++;
+                }
+            }while(num >= 0);
+
+            if(contador >0)
+            {
+                media = (double)soma/ contador;
+            }
+
+            Console.WriteLine("A média é: {0}", media);
+            Console.ReadKey();
+        }
+
+        static void Exercicio3()
+        {
+            Console.Write("Digite o valor de X como valor inicial: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o valor de Y como valor final: ");
+            int y = int.Parse(Console.ReadLine());
+
+            for(int i = x; i <= y; i++)
+            {
+                if (i % 7 == 3 || i % 7 == 5)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.ReadKey();
+        }
+
+        static void Exercicio4()
+        {
+            Console.Write("Digite um número P positivo: ");
+            int p = int.Parse(Console.ReadLine());
+
+            
+            Console.ReadKey();
+        }
+
+        static void Exercicio5()
+        {
+            Console.Write("Digite um número de linhas: ");
+            int numLinhas = int.Parse(Console.ReadLine());   
+
+            for(int i = 1; i <= numLinhas; i++)
+            {
+                for(int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+        static void Exercicio6()
+        {
+            Console.Write("Digite um número de linhas: ");
+            int numLinhas = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numLinhas; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+
+            int opcao = -1;
+
+
+            while (opcao != 0)
+            {
+                Console.Clear();
+                Console.WriteLine("           Bem-vindo a Lista01!\n\nEscolha a opção do exercício a ser executado");
+                Console.WriteLine("   __________________________________");
+                Console.Write("   01.....................Exercício01\n" +
+                    "   02.....................Exercício02\n" +
+                    "   03.....................Exercício03\n" +
+                    "   04.....................Exercício04\n" +
+                    "   05.....................Exercício05\n" +
+                    "   06.....................Exercício06\n" +
+                    "   0.............................Sair\n");
+                Console.WriteLine("   __________________________________\n");
+
+                Console.Write("Digite a opção: ");
+                opcao = int.Parse(Console.ReadLine());
+
+                switch (opcao)
+                {
+                    case 1:
+                        Exercicio1();
+                        break;
+                    case 2:
+                        Exercicio2();
+                        break;
+                    case 3:
+                        Exercicio3();
+                        break;
+                    case 4:
+                        Exercicio4();
+                        break;
+                    case 5:
+                        Exercicio5();
+                        break;
+                    case 6:
+                        Exercicio6();
+                        break;
+                    default:
+                        break;
+
+                }
+            }
+
+        }
+    }
+}
---
+++ Lista02/Lista02/README.md
@@ -0,0 +1,39 @@
+# Instituto Federal de São Paulo – Campus Campos do Jordão
+## Linguagem de Programação 2 – LP2A4
+<h3>Professores: Marques Sousa</h3>
+
+Questões de Programação
+
+1. Escreva um programa que obtenha 172 valores reais no intervalo [23, 9568], encontre o maior e o
+menor entre eles e, ao final, mostre a média aritmética de todos os valores pares e menor que 100,
+o menor e o maior número encontrado.
+2. Faça um programa que calcule a média de um conjunto de números digitados pelo usuário. O
+programa deve permitir que o usuário digite uma quantidade não determinada de números,
+encerrando-se quando o usuário digitar um número negativo. Apresente a média aritmética dos
+números ao final da entrada de dados.
+3. Escreva um programa que mostre os números que possuem resto igual a 3 ou 5, quando divididos
+por 7. Considere o intervalo entre X e Y, incluindo os extremos. Os valores das variáveis X e Y são
+informados pelo usuário.
+4. Faça um programa que leia um número P positivo e mostre na tela os P
+3 primeiros números ímpares
+que são ao mesmo tempo divisíveis por 4, mas não por 6.
+5. Crie um programa que seja capaz de apresentar uma figura similar à apresentada abaixo. O usuário
+entrará somente com o número de linhas e o caractere a ser utilizado na apresentação final. No
+exemplo, o número de linhas foi 5 e o caractere foi o asterisco.
+*
+**
+***
+****
+*****
+6. Crie um programa que seja capaz de apresentar uma figura similar à apresentada abaixo. O usuário
+entrará somente com o número de linhas e o caractere a ser utilizado na apresentação final. No
+exemplo, o número de linhas foi 5 e o caractere foi o asterisco.
+    *
+   ***
+  *****
+ *******
+*********
+
+<br>
+<br>
+<h3>BOA ATIVIDADE</h3>
