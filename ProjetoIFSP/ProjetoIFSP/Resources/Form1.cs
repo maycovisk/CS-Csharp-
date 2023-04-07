97725eee37de37d8057bb2789d25006ffada408a
Maycon Lima <90663036+maycovisk@users.noreply.github.com>
2023-04-06 22:19:21 -0300

    Add Projeto do Projeto

---
+++ ProjetoIFSP/ProjetoIFSP/Resources/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjetoIFSP.Resources
+{
+    public partial class loginPage : Form
+    {
+        public loginPage()
+        {
+            InitializeComponent();
+        }
+
+        private void btLogin_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
