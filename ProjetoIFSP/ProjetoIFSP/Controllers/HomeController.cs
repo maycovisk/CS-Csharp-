2d3c68755105469437f9da06a6681d7752ea3326
Maycon Lima <90663036+maycovisk@users.noreply.github.com>
2023-04-08 16:11:01 -0300

    Add ProjetoIFSP

---
+++ ProjetoIFSP/ProjetoIFSP/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProjetoIFSP.Models;
+using System.Diagnostics;
+
+namespace ProjetoIFSP.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+
+            home.Nome = "Maycon de Lima César";
+            home.Email = "lima.maycon@aluno.ifsp.edu.br";
+
+            return View(home);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ ProjetoIFSP/ProjetoIFSP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoIFSP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
